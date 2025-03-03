f9f42209ef5309644dee39595f5270759ed381b8
pniyongabo <patrickniyongabo@gmail.com>
2025-03-03 15:20:31 -0800

    minor change to handle PDF with images

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 click
 pdf2docx
 pdfminer.six - https://github.com/pdfminer/pdfminer.six/blob/master/README.md
+rapidocr-onnxruntime
 
 
 ## Other Tools
 - https://github.com/langchain-ai/langchain/blob/7c175e3fdaed71c9d031b7f62eabcade075695ed/libs/community/langchain_community/document_loaders/pdf.py#L186
-
-
+- Claude API support for PDFs: https://docs.anthropic.com/en/docs/build-with-claude/pdf-support
--- process-pdf-with-langchain.py
+++ process-pdf-with-langchain.py
@@ -3,9 +3,15 @@
 
 
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders.parsers.images import RapidOCRBlobParser
 
 # loader = PyPDFLoader("./testing_files/inputs/pdf-without-images-1-page.pdf")
-loader = PyPDFLoader("./testing_files/inputs/pdf-with-images.pdf")
+loader = PyPDFLoader(
+    file_path="./testing_files/inputs/pdf-with-images.pdf",
+    mode="single",  # vs "page"
+    extract_images=True,
+    images_parser=RapidOCRBlobParser(),
+)
 documents = loader.load()  # Loads entire PDF
 # documents = loader.lazy_load()  # Loads pages as needed
 
